310754f77eb8d78ba4c8794a3cb10ac958e7cd45
home <kulou322@126.com>
2018-09-18 23:34:32 +0800

    Array对象

--- ES5/标准库/Array 对象.js
+++ ES5/标准库/Array 对象.js
@@ -42,3 +42,41 @@ console.log(d);
 // reverse()
 // console.log(c);
 
+let dArr = [1,2,3,4,5,6,7,8]
+console.log(dArr.reverse());
+console.log(dArr);
+
+
+let eArr = dArr.slice(2,5)
+console.log(eArr);
+
+let fArr = dArr.splice(2,1,'a','b')
+console.log(dArr);
+console.log(fArr);
+
+dArr.sort((e1,e2) => e1 > e2)
+console.log(dArr);
+
+
+let gArr = dArr.map((value) => value + 4)
+console.log(gArr);
+
+let someFlag = dArr.some(value => type.isNumber(value))
+console.log(someFlag);
+
+let everyFlag = dArr.every(value => type.isNumber(value))
+console.log(everyFlag);
+
+let reduce = dArr.reduce((preValue,currentValue) => preValue + currentValue)
+console.log(reduce);
+
+let reduceRight = dArr.reduceRight( (preValue,currentValue) => preValue + currentValue)
+console.log(reduceRight);
+
+
+let index = dArr.indexOf(5)
+let lastindex = dArr.lastIndexOf(5)
+console.log(`${index},${lastindex}`);
+
+
+
