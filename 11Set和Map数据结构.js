38415f325344f33ff72f53dd9261ee01df67c19a
home <kulou322@126.com>
2018-11-08 00:11:40 +0800

    Set和Map数据结构

--- 11Set和Map数据结构.js
+++ 11Set和Map数据结构.js
@@ -8,12 +8,35 @@ let kk = JSON.parse(jsonStr)
 console.log(kk);
 
 
-let a = [1,2,3,4,5,6,7,8,9,0]
-let len = a.length - 1
+let a1 = [1,2,3,4,5,6,7,8,9,0]
+let len = a1.length - 1
 while (--len > 0) {
-    if (a[len] == 4 ) {
-        a.splice(len,1)
+    if (a1[len] == 4 ) {
+        a1.splice(len,1)
         break
     }
 }
-console.log(...a);
+console.log(...a1);
+
+// Set 函数可以接受一个数组（或者具有 iterable 接口的其他数据结构）作为参数，用来初始化。
+let s1 = new Set([1,2,3,4,4,5,6,6,7])
+console.log(s1);
+
+console.log(...new Set([1,1,1,1,1,3]));
+
+
+let s2 = new Set()
+s2.add({})
+s2.add({})
+console.log(s2);
+
+let s3 = new Set([1,2,3,4,5,5,6,7,8,9,9])
+s3.add(2)
+s3.delete(3)
+console.log(s3.has(4));
+s3.clear()
+console.log(s3);
+
+
+const a4 = Array.from(new Set([1,2,3,4,4,5,6]))
+console.log(a4);
